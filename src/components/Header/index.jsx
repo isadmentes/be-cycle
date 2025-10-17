9ed2d7f384e7ace55d7f9d217c475178bb38c640
isadmentes <isadmentes@gmail.com>
2025-10-16 21:58:20 -0300

    Estilizado o header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export const Header = () => {
+    return (
+        <header className="bg-green-800 p-8">
+            <h2 className = "text-neutral-50 text-3xl text-center font-bold mb-4">Be-cycle</h2>
+            <ul className="flex flex-col md:flex-row items-center text-neutral-50 text-lg font-bold justify-between">
+                <li> <Link to="/">Inicio </Link></li>
+                <li> <Link to="/">Produtos </Link></li>
+                <li> <Link to="/">Empresas </Link></li>
+                <li> <Link to="/">Tecnologias </Link></li>
+
+            </ul>
+        </header>
+    )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,30 @@
+import { Header } from "../../components/Header";
+
 export const HomePage = () => {
     return (
         <main className="min-h-screen bg-neutral-50" >
-            <article className="lg:max-w-5xl mx-auto py-12 px-8">
+            <Header />
 
-                <h1 className="text-amber-800 text-4xl text-center font-bold mb-30">
+            <article className="w-full max-w-5xl mx-auto flex flex-col items-center  py-18 px-10">
+                <h1 className="text-amber-800 text-4xl font-bold text-center mb-12">
                     Inovação e circularidade para uma indústria regenerativa
                 </h1>
 
-                <h2 className="text-blue-900 text-xl text-center">
-                    Nos sistemas convencionais, toneladas de subprodutos como
-                    cascas, sementes, polpas, bagaços e efluentes são descartadas,
-                    gerando impacto ambiental e custos de manejo. No modelo 5.0,
-                    esses materiais passam a ser matérias-primas secundárias,
-                    processadas por rotas tecnológicas limpas e conectadas a
-                    produtivas inteligentes. A aplicação de tecnologias verdes
-                    permite recuperar compostos bioativos, fibras, óleos
-                    e biopolímeros com aplicação nos setores alimentício,
-                    cosmético, farmacêutico e de bioplásticos.
-                </h2>
+                <div className="grid md:grid-cols-2 items-center gap-8">
+                    <img src="./figures/economia-circular-emf-ciclovivo-1024x696.webp" />
+
+                    <h2 className="text-black text-xl md:max-w-3xl text-justify">
+                        Nos sistemas convencionais, <a class="underline decoration-sky-500"> toneladas de subprodutos </a> como
+                        cascas, sementes, polpas, bagaços e efluentes <a class="underline decoration-sky-500"> são descartadas </a>,
+                        gerando impacto ambiental e custos de manejo. No modelo 5.0,
+                        esses materiais passam a ser matérias-primas secundárias,
+                        processadas por rotas tecnológicas limpas e conectadas a
+                        produtivas inteligentes. A aplicação de tecnologias verdes
+                        permite <a class="underline decoration-sky-500"> recuperar </a> compostos bioativos, fibras, óleos
+                        e biopolímeros com aplicação nos setores <a class="underline decoration-sky-500"> alimentício,
+                            cosmético, farmacêutico e de bioplásticos </a>.
+                    </h2>
+                </div>
             </article>
         </main>
     );
