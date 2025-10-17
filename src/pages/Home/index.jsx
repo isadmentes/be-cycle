705737eec1ae0385923c8715ecbdc8ba4aacb1b5
isadmentes <isadmentes@gmail.com>
2025-10-16 21:17:42 -0300

    Estilizado a Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,24 @@
 export const HomePage = () => {
-  return (
-    <main className="min-h-dvh grid place-items-center bg-slate-50">
-      <h1 className="text-3xl font-bold text-slate-800">
-        Home Page
-      </h1>
-    </main>
-  );
+    return (
+        <main className="min-h-screen bg-neutral-50" >
+            <article className="lg:max-w-5xl mx-auto py-12 px-8">
+
+                <h1 className="text-amber-800 text-4xl text-center font-bold mb-30">
+                    Inovação e circularidade para uma indústria regenerativa
+                </h1>
+
+                <h2 className="text-blue-900 text-xl text-center">
+                    Nos sistemas convencionais, toneladas de subprodutos como
+                    cascas, sementes, polpas, bagaços e efluentes são descartadas,
+                    gerando impacto ambiental e custos de manejo. No modelo 5.0,
+                    esses materiais passam a ser matérias-primas secundárias,
+                    processadas por rotas tecnológicas limpas e conectadas a
+                    produtivas inteligentes. A aplicação de tecnologias verdes
+                    permite recuperar compostos bioativos, fibras, óleos
+                    e biopolímeros com aplicação nos setores alimentício,
+                    cosmético, farmacêutico e de bioplásticos.
+                </h2>
+            </article>
+        </main>
+    );
 }
